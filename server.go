f4212a24cee4323ca01248d710b074e3215a89c5
t0pep0 <t0pep0.gentoo@gmail.com>
2014-07-25 13:46:28 +0400

    inital

---
+++ server.go
@@ -0,0 +1,111 @@
+package httpKit
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	"time"
+)
+
+//HTTP Server interface
+type Server interface {
+	//Set server host & port
+	Configure(host, port string, logFunc func(http.Handler) http.Handler)
+	//Launch server
+	Run() error
+	//Get router
+	GetRouter() *mux.Router
+	//Add route
+	AddRoute(pattern, method, name string, handler func(http.ResponseWriter, *http.Request))
+	//404 error route
+	Set404Route(handler func(http.ResponseWriter, *http.Request))
+	//Get server status
+	Status() (configured, run bool)
+}
+
+type sError string
+
+func (err *sError) Error() string {
+	return string(*err)
+}
+
+type err404Handler struct {
+	fn404 func(http.ResponseWriter, *http.Request)
+}
+
+func (e *err404Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	e.fn404(w, r)
+}
+
+type server struct {
+	server     *http.Server
+	host, port string
+	router     *mux.Router
+	logFunc    func(http.Handler) http.Handler
+	configured bool
+	run        bool
+	err404     *err404Handler
+}
+
+func (s *server) log(handler http.Handler) (ret http.Handler) {
+	if s.logFunc != nil {
+		return s.logFunc(handler)
+	}
+	return handler
+}
+
+func New() (serv Server) {
+	servp := new(server)
+	servp.router = mux.NewRouter()
+	servp.configured = false
+	servp.run = false
+	servp.err404 = new(err404Handler)
+	return servp
+}
+
+func (s *server) Configure(host, port string, logFunc func(http.Handler) http.Handler) {
+	s.host = host
+	s.port = port
+	s.logFunc = logFunc
+	s.configured = true
+}
+
+func (s *server) Run() error {
+	err := new(sError)
+	if s.run {
+		*err = sError("Server already running")
+		return err
+	}
+	if s.configured {
+		http.Handle("/", s.router)
+		s.server = &http.Server{
+			Addr:           s.host + ":" + s.port,
+			Handler:        s.log(http.DefaultServeMux),
+			ReadTimeout:    100 * time.Second,
+			WriteTimeout:   100 * time.Second,
+			MaxHeaderBytes: 1 << 20,
+		}
+		go s.server.ListenAndServe()
+		s.run = true
+	} else {
+		*err = sError("Server not configured!")
+		return err
+	}
+	return err
+}
+
+func (s *server) GetRouter() (router *mux.Router) {
+	return s.router
+}
+
+func (s *server) AddRoute(pattern, method, name string, handler func(http.ResponseWriter, *http.Request)) {
+	s.router.HandleFunc(pattern, handler).Methods(method).Name(name)
+}
+
+func (s *server) Set404Route(handler func(http.ResponseWriter, *http.Request)) {
+	s.err404.fn404 = handler
+	s.router.NotFoundHandler = s.err404
+}
+
+func (s *server) Status() (configured, run bool) {
+	return s.configured, s.run
+}
---
+++ server_test.go
@@ -0,0 +1,108 @@
+package httpKit
+
+import (
+	"net/http"
+	"testing"
+)
+
+var (
+	server_test Server
+)
+
+func TestNew(t *testing.T) {
+	server_test = New()
+	if server_test == nil {
+		t.Fatal("Server is nil!")
+	} else {
+		t.Log("Server created ..OK!")
+	}
+}
+
+func TestRunWithoutConfig(t *testing.T) {
+	err := server_test.Run()
+	if err.Error() != "Server not configured!" {
+		t.Fatal("Server.Run() not return error ..FAIL!")
+	} else {
+		t.Log("Server.Run return error:", err.Error(), "..OK!")
+	}
+}
+
+func TestRunWithConfig(t *testing.T) {
+	test_log := make(chan bool, 10)
+	log_func := func(handler http.Handler) http.Handler {
+		test_log <- true
+		return handler
+	}
+	server_test.Configure("localhost", "9999", log_func)
+	err := server_test.Run()
+	if err.Error() != "" {
+		t.Fatal("Server.Run() return error:", err.Error(), " ..FAIL!")
+	} else {
+		t.Log("Server.Run() ..OK!")
+	}
+	test, ok := <-test_log
+	if (!test) || (!ok) {
+		t.Fatal("Server.SetLogFunc() not work ..FAIL!")
+	} else {
+		t.Log("Server.SetLogFunc() work ..OK!")
+	}
+}
+
+func TestDoubleRun(t *testing.T) {
+	err := server_test.Run()
+	if err.Error() != "Server already running" {
+		t.Fatal("Server.Run() not return err ..FAIL!")
+	} else {
+		t.Log("Server.Run() return error:", err.Error(), "..OK!")
+	}
+}
+
+func TestRoute(t *testing.T) {
+	test_chan := make(chan bool, 10)
+	test_handler := func(w http.ResponseWriter, r *http.Request) {
+		test_chan <- true
+	}
+	server_test.AddRoute("/test_route", "GET", "TestRoute", test_handler)
+	http.Get("http://localhost:9999/test_route")
+	test, ok := <-test_chan
+	if (!test) || (!ok) {
+		t.Fatal("Server.AddRoute() not work ..FAIL!")
+	} else {
+		t.Log("Server.AddRoute() work ..OK!")
+	}
+}
+
+func TestGetRouter(t *testing.T) {
+	router := server_test.GetRouter()
+	if router == nil {
+		t.Fatal("Server.GetRouter() not return router ..FAIL!")
+	} else {
+		t.Log("Server.GetRouter() return router ..OK!")
+	}
+}
+
+func Test404Route(t *testing.T) {
+	test_chan := make(chan bool, 2)
+	test_handler := func(w http.ResponseWriter, r *http.Request) {
+		test_chan <- true
+	}
+	server_test.Set404Route(test_handler)
+	http.Get("http://localhost:9999/404")
+	test, ok := <-test_chan
+	if (!test) || (!ok) {
+		t.Fatal("Server.Set404Route() not work ..FAIL!")
+	} else {
+		t.Log("Server.Set404Route work! ..OK!")
+	}
+}
+
+func TestStatus(t *testing.T) {
+	conf, run := server_test.Status()
+	if !conf {
+		t.Fatal("Incorrect configure status! ..FAIL!")
+	}
+	if !run {
+		t.Fatal("Incorrect run status! ..FAIL!")
+	}
+	t.Log("Server.Status() work! ..OK!")
+}
